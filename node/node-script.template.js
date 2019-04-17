0c00d2efa15e48bfdce6e6c2208fbaa054a96a8b
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-04-16 18:10:06 -0600

    Start a Node Script Template Document

---
+++ node/node-script.template.js
@@ -0,0 +1,12 @@
+/*
+# Node Script Template
+
+This template contains a bunch of code snippets you may find useful in your next
+node script.
+
+Note: What is found here generally does not apply to library style code. Library
+style code is usually characterized by the fact that a module only exports
+declarations and does not have side-effects. Scripts, on the otherhand, do
+generally have side-effects and usually don't export anything.
+*/
+
