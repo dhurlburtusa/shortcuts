f5ec52ae4c3f8c79fe2506db0db5733c15e30dbd
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2019-06-30 12:17:43 -0600

    Update Function/Variable Names

--- wordpress/site-specific-plugin.template.php
+++ wordpress/site-specific-plugin.template.php
@@ -7,25 +7,25 @@ Text Domain: hsllc
 Domain Path: /languages
 */
 
-if (!defined('SSP_DIRNAME')) {
-  define('SSP_DIRNAME', plugin_basename(dirname(__FILE__)));
+if (!defined('SSP__DIRNAME')) {
+  define('SSP__DIRNAME', plugin_basename(dirname(__FILE__)));
 }
-define('SSP_PATH', plugin_dir_path(__FILE__));
+define('SSP__PATH', plugin_dir_path(__FILE__));
 
-function ssp_on_activation () {
+function ssp__on_activation () {
   // TODO: Add options (optional).
   // TODO: Add tables (optional).
 }
-register_activation_hook( __FILE__, 'ssp_on_activation' );
+register_activation_hook( __FILE__, 'ssp__on_activation' );
 
-function ssp_on_deactivation () {
+function ssp__on_deactivation () {
   // TODO: Flush Cache/Temp
   // TODO: Flush Permalinks
 }
-register_deactivation_hook( __FILE__, 'ssp_on_deactivation' );
+register_deactivation_hook( __FILE__, 'ssp__on_deactivation' );
 
-function ssp_on_uninstall () {
+function ssp__on_uninstall () {
   // TODO: Remove any added options.
   // TODO: Remove any added tables.
 }
-register_uninstall_hook( __FILE__, 'ssp_on_uninstall');
+register_uninstall_hook( __FILE__, 'ssp__on_uninstall');
